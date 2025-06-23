bbdf0bff9a7a89deddad6611c8413949adc5fb40
unknown <Vitorhugolimadeoliveira235@gmail.com>
2025-06-23 09:02:32 -0300

    trabalhando 2

---
+++ backend/logicaProgamacao/01_variaveis.py
@@ -0,0 +1,38 @@
+nome = "Ana"
+idade = 25
+altura = 1.68
+tem_carteira = True
+
+print("Nome", nome)
+print("Idade", idade)
+print("Altura", altura)
+print("Tem carteira?", tem_carteira)
+
+animal = "Macaco"
+trabalho = "repositor"
+
+print("animal", macaco)
+print("trabalho", repositor)
+
+numero = 10
+ano = 2007
+
+print("numero", numero)
+print("ano", ano)
+
+peso = 60 km
+temperatura = 29.1
+
+print("velocidade", velocidade)
+print("temperatura", temperatura)
+
+tem_sede = False
+breno_macaco = True
+
+print("Tem sede?", tem_sede)
+print("Breno é macaco?", breno_macaco)
+
+
+
+
+
