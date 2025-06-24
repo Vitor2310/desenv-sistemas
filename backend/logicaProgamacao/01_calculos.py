9a3db4c3cde802f762c8cddcdd4f0dd15fcf59d0
Vitor <Vitorhugolimadeoliveira235@gmail.com>
2025-06-24 09:32:25 -0300

    Exercicios condicionais

---
+++ backend/logicaProgamacao/01_calculos.py
@@ -0,0 +1,9 @@
+valor1 = int (input("Digite um valor "))
+valor2 = int (input("Digite um valor "))
+
+
+if (valor1 > valor2):
+        print("Valor1 é maior", valor1)
+elif (valor1 < valor2):
+        print("(valor2 é maior", valor2)
+
