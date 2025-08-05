a48556f8d0d3ec0bd16417b82e2b1eb87ae25dfb
Vitor <Vitorhugolimadeoliveira235@gmail.com>
2025-08-05 09:40:18 -0300

    msg

---
+++ backend/logicaProgamacao/06_condicionais.py
@@ -0,0 +1,12 @@
+#crie uma funcao que faça a media de 3 valores
+#crie uma função que calcule o imposto anualdo seu salario
+#crie uma função que valde se a senha desafio esta correta
+
+def calculomedia(valor1, valor2, valor3):
+    calculomedia = (valor1 + valor2 + valor3) / 3
+    print ("Media: media")
+
+    def impostoanual(salario_mensal):
+        salario_anual = salario_mensal * 12
+        imposto = salario_anual * 0.15
+        print("imposto mensal", impostoanual)
