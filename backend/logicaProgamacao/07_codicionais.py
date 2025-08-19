f43a483f6f793807a11ce3a13685c63975c8d6d7
Vitor <Vitorhugolimadeoliveira235@gmail.com>
2025-08-19 09:40:14 -0300

    msg

---
+++ backend/logicaProgamacao/07_codicionais.py
@@ -0,0 +1,15 @@
+#append pro final
+#susert 1,1700,00
+tarefas = [
+    "lavar a louça",
+    "estudar matemática",
+    "fazer compras",
+    "limpar o quarto",
+    "pagar contas"
+]
+
+tarefas.insert(2, "ir viajar")
+
+tarefas.remove("lavar a louça")
+
+
