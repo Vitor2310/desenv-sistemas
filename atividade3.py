b4cc3b1611218c8cb0a3473cea50ec97a2f15bb4
Vitor <Vitorhugolimadeoliveira235@gmail.com>
2025-09-09 11:38:37 -0300

    msg

--- atividade3.py
+++ atividade3.py
@@ -1,1 +1,9 @@
 # explique porque quando tenha mais de um atributo(variável), torna-se dificil/complicado o uso de arquivos txt para guardar informações
+
+#Quando há muitos atributos (variáveis), usar arquivos .txt se torna complicado porque:
+#Falta de estrutura — Não garante que todas as linhas tenham os mesmos dados.
+#Inconsistência — Dados podem estar desorganizados ou incompletos.
+#Leitura difícil — Requer mais código para interpretar corretamente.
+#Pouco prático em larga escala — Difícil de buscar e manter com muitos registros.
+#Sem tipos de dados — Tudo é tratado como texto, o que limita a organização.
+# Por isso, é melhor usar CSV, JSON ou banco de dados, que são mais estruturados e confiáveis.
